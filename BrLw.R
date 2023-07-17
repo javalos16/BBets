e60c5a9aae58e0429b18689e48f57f42260a18a9
jamesbrownlow <jbrownlow@avc.edu>
2023-07-17 22:24:19 +0000

    set up

--- BrLw.R
+++ BrLw.R
@@ -1,15 +1,20 @@
-BrLw <-function(numClass,odds,countoDo){
+BrLw <-function(numInClass,odds){
 
-days=1:365
-count=countoDo
-repeatn=numClass
-varstor=numeric()
+  days=1:365
+  varstor=numeric()
   brownlow = 0
-  for (i in 1:countoDo) {
-    sam=sample(days,size=numClass,replace = T)
+  
+  for (i in 1:1000) {
+    sam=sample(days,size=numInClass,replace = T)
     y=duplicated(sam)
     if (sum(y)>0) brownlow=brownlow+1
-    else brownlow = brownlow - odds
+    #else brownlow = brownlow - odds
   }
- return(brownlow)
+  pWin = brownlow/1000
+  
+  bWins = numInClass/2 * pWin * 1
+  bLoss = numInClass/2 * (1 - pWin)*odds
+  
+ return(bWins-bLoss)
 }
+
